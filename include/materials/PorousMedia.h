d411a29e213b5d359c705db49472a6a7c9ee0c84
Hai Huang <hai.huang@inl.gov>
2010-03-01 15:54:09 +0000

    continue re-structure materials
    r1598

---
+++ include/materials/PorousMedia.h
@@ -0,0 +1,46 @@
+#ifndef POROUSMEDIA_H
+#define POROUSMEDIA_H
+
+#include "Material.h"
+
+
+//Forward Declarations
+class PorousMedia;
+
+template<>
+InputParameters validParams<PorousMedia>();
+
+/**
+ * Simple material with PorousMedia properties.
+ */
+class PorousMedia : public Material
+{
+public:
+  PorousMedia(std::string name,
+           InputParameters parameters,
+           unsigned int block_id,
+           std::vector<std::string> coupled_to,
+           std::vector<std::string> coupled_as);
+  
+protected:
+  virtual void computeProperties();
+
+  Real _input_permeability;
+  Real _input_porosity;
+  Real _input_rho_r;
+
+  Real _input_gravity;
+  Real _gx;
+  Real _gy;
+  Real _gz;
+
+  std::vector<Real> & _permeability;
+  std::vector<Real> & _porosity;
+  std::vector<Real> & _rho_r;
+  
+  std::vector<Real> & _gravity;
+  std::vector<RealVectorValue> & _gravity_vector;
+
+};
+
+#endif //POROUSMEDIA_H
