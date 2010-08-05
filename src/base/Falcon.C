0647e867400f94b7d0e4e618a2cd325302020f68
Derek Gaston <derek.gaston@inl.gov>
2010-08-05 14:35:22 +0000

    initial displaced mesh capability
    r2167

--- src/base/Falcon.C
+++ src/base/Falcon.C
@@ -69,7 +69,7 @@
 #include "PressureNeumannBC.h"
 #include "GravityNeumannBC.h"
 #include "OutFlowBC.h"
-
+#include "HuyakornEx1BC.h"
 
 #include "Constant.h"
 #include "DarcyWater.h"
@@ -152,6 +152,7 @@ namespace Falcon
     BCFactory::instance()->registerBC<PressureNeumannBC>("PressureNeumannBC");
     BCFactory::instance()->registerBC<GravityNeumannBC>("GravityNeumannBC");
     BCFactory::instance()->registerBC<OutFlowBC>("OutFlowBC");
+    BCFactory::instance()->registerBC<HuyakornEx1BC>("HuyakornEx1BC");
 
     MaterialFactory::instance()->registerMaterial<Constant>("Constant");
     MaterialFactory::instance()->registerMaterial<DarcyWater>("DarcyWater");
