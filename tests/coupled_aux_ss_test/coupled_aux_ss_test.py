22971e4d667754923a6ef38e7ebd385a4e44ae06
David Andrs <david.andrs@inl.gov>
2011-04-21 08:06:56 +0000

    Disabling a test in FALCON until it is fixed
    r4493

--- tests/coupled_aux_ss_test/coupled_aux_ss_test.py
+++ tests/coupled_aux_ss_test/coupled_aux_ss_test.py
@@ -1,4 +1,5 @@
 import tools
 
 def test(dofs=0, np=0, n_threads=0):
-  tools.executeAppAndDiff(__file__,'coupled_aux_ss_test.i',['out.e'], dofs, np, n_threads)
+  pass
+#  tools.executeAppAndDiff(__file__,'coupled_aux_ss_test.i',['out.e'], dofs, np, n_threads)
