95118dd1abe38db5cdce30734989cc398adc5641
Cody Permann <cody.permann@inl.gov>
2012-01-10 15:44:29 +0000

    Removing duplicate tests from Falcon - trigger new build in moose Ticket #000
    r8030

--- tests/element_aux_var_test/element_aux_var_test.py
+++
@@ -1,5 +0,0 @@
-from options import *
-
-test = { INPUT : 'element_aux_var_test.i',
-         EXODIFF : ['out.e']}
-
--- tests/nodal_aux_var_test/nodal_aux_var_test.py
+++
@@ -1,5 +0,0 @@
-from options import *
-
-test = { INPUT : 'nodal_aux_var_test.i',
-         EXODIFF : ['out.e']}
-
