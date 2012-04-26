afc663a97ff2c3f5552beb64392298f3ac571f9e
Cody Permann <cody.permann@inl.gov>
2012-04-26 10:26:25 +0000

    Adding clobber target in support of multiple test runs - refs #999
    r10237

--- falcon.mk
+++ falcon.mk
@@ -64,6 +64,12 @@ endif
 clean::
 	@rm -fr $(falcon_APP)
 	@rm -fr $(falcon_LIB)
+	@find . \( -name "*~" -or -name "*.o" -or -name "*.d" -or -name "*.pyc" \) -exec rm '{}' \;
+	@rm -fr *.mod
+
+clobber::
+	@rm -fr $(falcon_APP)
+	@rm -fr $(falcon_LIB)
 	@find . \( -name "*~" -or -name "*.o" -or -name "*.d" -or -name "*.pyc" \
                 -or -name "*.gcda" -or -name "*.gcno" -or -name "*.gcov" \) -exec rm '{}' \;
 	@rm -fr *.mod
