950c0134c1c5d9abee2ed60870d881a716e8426b
Katherine Smith <katherine.smith@inl.gov>
2012-11-06 14:00:51 +0000

    adjusting CSVDIFF relative error on EOS tests
    r14750

--- tests/UserObjectTests/EOSViscosityFunc_test/EOSViscosityFunc_test.py
+++ tests/UserObjectTests/EOSViscosityFunc_test/EOSViscosityFunc_test.py
@@ -2,5 +2,4 @@ from options import *
 
 test = { INPUT : 'EOSViscosityFunc_test.i',
          CSVDIFF : ['EOSViscosityFunc_test_out.csv'],
-         REL_ERR : 7.5e-2,
          TYPE : 'CSVDIFF' }
--- tests/UserObjectTests/EOSWaterAndSteamPTFunc_test/EOSWaterAndSteamPTFunc_test.py
+++ tests/UserObjectTests/EOSWaterAndSteamPTFunc_test/EOSWaterAndSteamPTFunc_test.py
@@ -2,20 +2,16 @@ from options import *
 
 test1 = { INPUT : 'DensityWater_PT_test.i',
          CSVDIFF : ['DensityWater_PT_test_out.csv'],
-         REL_ERR : 5e-5,
          TYPE : 'CSVDIFF'}
 
 test2 = { INPUT : 'DensitySteam_PT_test.i',
           CSVDIFF : ['DensitySteam_PT_test_out.csv'],
-          REL_ERR : 8e-4,
           TYPE : 'CSVDIFF'}
 
 test3 = { INPUT : 'EnthalpyWater_PT_test.i',
           CSVDIFF : ['EnthalpyWater_PT_test_out.csv'],
-          REL_ERR : 5e-4,
           TYPE : 'CSVDIFF'}
 
 test4 = { INPUT : 'EnthalpySteam_PT_test.i',
           CSVDIFF : ['EnthalpySteam_PT_test_out.csv'],
-          REL_ERR : 5e-4,
           TYPE : 'CSVDIFF'}
