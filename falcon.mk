3832b664a7432c5dfb6278d7f5bd9c2498484d10
John Peterson <jw.peterson@inl.gov>
2013-01-18 11:46:07 +0000

    Updating apps to use automake libmesh.
    r15945

--- falcon.mk
+++ falcon.mk
@@ -5,7 +5,7 @@ falcon_INCLUDE  := $(foreach i, $(falcon_INC_DIRS), -I$(i))
 
 libmesh_INCLUDE := $(falcon_INCLUDE) $(libmesh_INCLUDE)
 
-falcon_LIB := $(FALCON_DIR)/libfalcon-$(METHOD)$(libext)
+falcon_LIB := $(FALCON_DIR)/libfalcon-$(METHOD).la
 
 falcon_APP := $(FALCON_DIR)/falcon-$(METHOD)
 
@@ -14,13 +14,12 @@ falcon_srcfiles    := $(shell find $(falcon_SRC_DIRS) -name *.C)
 falcon_csrcfiles   := $(shell find $(falcon_SRC_DIRS) -name *.c)
 falcon_fsrcfiles   := $(shell find $(falcon_SRC_DIRS) -name *.f)
 falcon_f90srcfiles := $(shell find $(falcon_SRC_DIRS) -name *.f90)
+
 # object files
 falcon_objects	:= $(patsubst %.C, %.$(obj-suffix), $(falcon_srcfiles))
 falcon_objects	+= $(patsubst %.c, %.$(obj-suffix), $(falcon_csrcfiles))
 falcon_objects += $(patsubst %.f, %.$(obj-suffix), $(falcon_fsrcfiles))
 falcon_objects += $(patsubst %.f90, %.$(obj-suffix), $(falcon_f90srcfiles))
-
-falcon_app_objects := $(patsubst %.C, %.$(obj-suffix), $(FALCON_DIR)/src/main.C)
 
 # plugin files
 falcon_plugfiles   := $(shell find $(FALCON_DIR)/plugins/ -name *.C 2>/dev/null)
@@ -34,29 +33,32 @@ falcon_plugins     += $(patsubst %.c, %-$(METHOD).plugin, $(falcon_cplugfiles))
 falcon_plugins     += $(patsubst %.f, %-$(METHOD).plugin, $(falcon_fplugfiles))
 falcon_plugins     += $(patsubst %.f90, %-$(METHOD).plugin, $(falcon_f90plugfiles))
 
+# falcon main
+falcon_main_src    := $(FALCON_DIR)/src/main.C
+falcon_app_objects := $(patsubst %.C, %.$(obj-suffix), $(falcon_main_src))
+
+# dependency files
+falcon_deps := $(patsubst %.C, %.$(obj-suffix).d, $(falcon_srcfiles)) \
+               $(patsubst %.c, %.$(obj-suffix).d, $(falcon_csrcfiles)) \
+               $(patsubst %.C, %.$(obj-suffix).d, $(falcon_main_src))
+
+# If building shared libs, make the plugins a dependency, otherwise don't.
+ifeq ($(libmesh_shared),yes)
+  falcon_plugin_deps := $(falcon_plugins)
+else
+  falcon_plugin_deps :=
+endif
+
 all:: $(falcon_LIB)
 
-# build rule for lib FALCON
-ifeq ($(enable-shared),yes)
-# Build dynamic library
-$(falcon_LIB): $(falcon_objects) $(falcon_plugins)
+$(falcon_LIB): $(falcon_objects) $(falcon_plugin_deps)
 	@echo "Linking "$@"..."
-	@$(libmesh_CC) $(libmesh_CXXSHAREDFLAG) -o $@ $(falcon_objects) $(libmesh_LDFLAGS)
-else
-# Build static library
-ifeq ($(findstring darwin,$(hostos)),darwin)
-$(falcon_LIB): $(falcon_objects)
-	@echo "Linking "$@"..."
-	@libtool -static -o $@ $(falcon_objects)
-else
-$(falcon_LIB): $(falcon_objects)
-	@echo "Linking "$@"..."
-	@$(AR) rv $@ $(falcon_objects)
-endif
-endif
+	@$(libmesh_LIBTOOL) --tag=CXX $(LIBTOOLFLAGS) --mode=link --quiet \
+	  $(libmesh_CXX) $(libmesh_CXXFLAGS) -o $@ $(falcon_objects) $(libmesh_LIBS) $(libmesh_LDFLAGS) $(EXTERNAL_FLAGS) -rpath $(FALCON_DIR)
+	@$(libmesh_LIBTOOL) --mode=install --quiet install -c $(falcon_LIB) $(FALCON_DIR)
 
 # include FALCON dep files
--include $(FALCON_DIR)/src/*/*.d
+-include $(falcon_deps)
 
 
 # how to build FALCON application
@@ -67,12 +69,12 @@ falcon: $(falcon_APP)
 
 $(falcon_APP): $(moose_LIB) $(elk_MODULES) $(falcon_LIB) $(falcon_app_objects)
 	@echo "Linking "$@"..."
-	@$(libmesh_CXX) $(libmesh_CXXFLAGS) $(falcon_app_objects) -o $@  $(falcon_LIB) $(elk_MODULES) $(moose_LIB) $(libmesh_LIBS) $(libmesh_LDFLAGS) $(ADDITIONAL_LIBS)
+	@$(libmesh_LIBTOOL) --tag=CXX $(LIBTOOLFLAGS) --mode=link --quiet \
+          $(libmesh_CXX) $(libmesh_CXXFLAGS) -o $@ $(falcon_app_objects) $(falcon_LIB) $(elk_MODULES) $(moose_LIB) $(libmesh_LIBS) $(libmesh_LDFLAGS) $(ADDITIONAL_LIBS)
 
--include $(FALCON_DIR)/src/*.d
 endif
 
-delete_list := $(falcon_APP) $(falcon_LIB)
+delete_list := $(falcon_APP) $(falcon_LIB) $(FALCON_DIR)/libfalcon-$(METHOD).*
 
 cleanall:: 
 	make -C $(FALCON_DIR) clean 
@@ -80,6 +82,6 @@ cleanall::
 ###############################################################################
 # Additional special case targets should be added here
 
-# F90 module dependancy rules
+# F90 module dependency rules
 $(FALCON_DIR)/src/Water_Steam_EOS/water_steam_phase_prop.f90: $(FALCON_DIR)/src/Water_Steam_EOS/water_eos.$(obj-suffix)
 $(FALCON_DIR)/src/Water_Steam_EOS/water_steam_phase_prop.f90: $(FALCON_DIR)/src/Water_Steam_EOS/IAPWS97.$(obj-suffix)
