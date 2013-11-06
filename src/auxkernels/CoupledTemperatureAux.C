d6137d029129c03574d1142cd1db58ba68181dc1
Cody Permann <cody.permann@inl.gov>
2013-11-06 13:11:15 +0000

    Revert "Continue the hacks for the sake of consistency... This patch will compute the Auxiliary system if there is a GeneralPostprocessor that couples in an aux variable - closes #2290"
    This reverts commit 71e9a1012d08cdbbabc8bbc40802b1725d2714e3.
    
    r22734

--- src/auxkernels/CoupledTemperatureAux.C
+++ src/auxkernels/CoupledTemperatureAux.C
@@ -19,8 +19,8 @@ template<>
 InputParameters validParams<CoupledTemperatureAux>()
 {
     InputParameters params = validParams<AuxKernel>();
-    params.addRequiredCoupledVar("pressure", "Use pressure to calculate variable temperature, [Pa]");
-    params.addRequiredCoupledVar("enthalpy", "Use enthalpy to calculate variable temperature, [J]");
+    params.addCoupledVar("pressure", "Use pressure to calculate variable temperature, [Pa]");
+    params.addCoupledVar("enthalpy", "Use enthalpy to calculate variable temperature, [J]");
     params.addParam("temperature", 373.15, "Initial value for temperature, [K]");
     //params.addParam("temp_guess", "Guess value for temperature to seed the EOS, [K]");
     params.addRequiredParam<UserObjectName>("water_steam_properties", "EOS functions, calculate water and steam properties");
@@ -42,8 +42,8 @@ CoupledTemperatureAux::computeValue()
 {
     Real _temperature = _input_temperature;
     Real _dummy_var[12];
-    
-    if (_t_step <= 1)
+
+    if (_t_step == 1)
     {
           _water_steam_properties.waterAndSteamEquationOfStatePropertiesPH (_enthalpy[_qp], _pressure[_qp], _input_temperature, _dummy_var[0] , _temperature, _dummy_var[1], _dummy_var[2], _dummy_var[3], _dummy_var[4], _dummy_var[5], _dummy_var[6], _dummy_var[7], _dummy_var[8], _dummy_var[9], _dummy_var[10], _dummy_var[11]);
     
