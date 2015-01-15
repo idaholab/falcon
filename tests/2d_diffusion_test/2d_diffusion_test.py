idaholab/moose

Test syntax converted to the new harness
r5321

test/tests/2d_diffusion_test/2d_diffusion_test.py
@@ -1,22 +1,10 @@
-import tools
+from options import *
 
-def testdirichlet(dofs=0, np=0, n_threads=0):
-  tools.executeAppAndDiff(__file__,'2d_diffusion_test.i',['out.e'], dofs, np, n_threads)
+testdirichlet = { INPUT : '2d_diffusion_test.i',
+                   EXODIFF : ['out.e'] }
 
-def testneumann(dofs=0, np=0, n_threads=0):
-  tools.executeAppAndDiff(__file__,'2d_diffusion_neumannbc_test.i',['neumannbc_out.e'], dofs, np, n_threads)
+testneumann   = { INPUT : '2d_diffusion_neumannbc_test.i',
+                   EXODIFF : ['neumannbc_out.e'] }
 
-
-try: # temporary to test the new test harness
-  from options import *
-
-  testdirichlet = { INPUT : '2d_diffusion_test.i',
-                     EXODIFF : ['out.e'] }
-
-  testneumann   = { INPUT : '2d_diffusion_neumannbc_test.i',
-                     EXODIFF : ['neumannbc_out.e'] }
-
-except:
-  pass
 
 

test/tests/nodal_aux_var_test/nodal_aux_var_test.py
@@ -1,26 +1,12 @@
-import tools
+from options import *
 
-def test(dofs=0, np=0, n_threads=0):
-  tools.executeAppAndDiff(__file__,'nodal_aux_var_test.i',['out.e'], dofs, np, n_threads)
+test = { INPUT : 'nodal_aux_var_test.i',
+         EXODIFF : ['out.e'] }
 
-#def ts_test(dofs=0, np=0, n_threads=0):
-#  tools.executeAppAndDiff(__file__,'nodal_aux_ts_test.i',['out_ts.e'], dofs, np, n_threads)
+ts_test = { INPUT : 'nodal_aux_ts_test.i',
+            EXODIFF : ['out_ts.e'],
+            SKIP : True }
 
-def multi_update_test(dofs=0, np=0, n_threads=0):
-  tools.executeAppAndDiff(__file__,'multi_update_var_test.i',['out_multi_var.e'], dofs, np, n_threads)
+multi_update_test = { INPUT : 'multi_update_var_test.i',
+                      EXODIFF : ['out_multi_var.e'] }
 
-try:
-  from options import *
-
-  test = { INPUT : 'nodal_aux_var_test.i',
-           EXODIFF : ['out.e'] }
-
-  ts_test = { INPUT : 'nodal_aux_ts_test.i',
-              EXODIFF : ['out_ts.e'],
-              SKIP : True }
-
-  multi_update_test = { INPUT : 'multi_update_var_test.i',
-                        EXODIFF : ['out_multi_var.e'] }
-
-except:
-  pass

test/tests/restart_diffusion_test/restart_diffusion_test.py
@@ -1,18 +1,9 @@
-import tools
+from options import *
 
-def test(dofs=0, np=0, n_threads=0):
-    tools.executeAppAndDiff(__file__,'restart_diffusion_test_steady.i',['steady_out.e'], dofs, np, n_threads)
-    tools.executeAppAndDiff(__file__,'restart_diffusion_test_transient.i',['out.e'], dofs, np, n_threads)
+test_1 = { INPUT : 'restart_diffusion_test_steady.i',
+           EXODIFF : ['steady_out.e'] }
 
-try:
-    from options import *
+test_2 = { INPUT : 'restart_diffusion_test_transient.i',
+           EXODIFF : ['out.e'],
+           PREREQ : 'test_1' }
 
-    test_1 = { INPUT : 'restart_diffusion_test_steady.i',
-               EXODIFF : ['steady_out.e'] }
-
-    test_2 = { INPUT : 'restart_diffusion_test_transient.i',
-               EXODIFF : ['out.e'],
-               PREREQ : 'test_1' }
-
-except:
-    pass

idaholab/falcon

tests/2d_diffusion_test/2d_diffusion_test.py
@@ -1,13 +1,5 @@
-import tools
+from options import *
 
-def test(dofs=0, np=0, n_threads=0):
-  tools.executeAppAndDiff(__file__,'2d_diffusion_test.i',['out.e'], dofs, np, n_threads)
+test = { INPUT : '2d_diffusion_test.i',
+         EXODIFF : ['out.e']}
 
-try:
-  from options import *
-
-  test = { INPUT : '2d_diffusion_test.i',
-           EXODIFF : ['out.e']}
-
-except:
-  pass

tests/nodal_aux_var_test/nodal_aux_var_test.py
@@ -1,13 +1,5 @@
-import tools
+from options import *
 
-def test(dofs=0, np=0, n_threads=0):
-  tools.executeAppAndDiff(__file__,'nodal_aux_var_test.i',['out.e'], dofs, np, n_threads)
+test = { INPUT : 'nodal_aux_var_test.i',
+         EXODIFF : ['out.e']}
 
-try:
-  from options import *
-
-  test = { INPUT : 'nodal_aux_var_test.i',
-           EXODIFF : ['out.e']}
-
-except:
-  pass

tests/restart_diffusion_test/restart_diffusion_test.py
@@ -1,17 +1,8 @@
-import tools
+from options import *
 
-def test(dofs=0, np=0, n_threads=0):
-    tools.executeAppAndDiff(__file__,'restart_diffusion_test_steady.i',['steady_out.e'], dofs, np, n_threads)
-    tools.executeAppAndDiff(__file__,'restart_diffusion_test_transient.i',['out.e'], dofs, np, n_threads)
+test1 = { INPUT : 'restart_diffusion_test_steady.i',
+         EXODIFF : ['steady_out.e']}
+test2 = { INPUT : 'restart_diffusion_test_transient.i',
+        EXODIFF : ['out.e'],
+        PREREQ : 'test1' }
 
-try:
-    from options import *
-
-    test1 = { INPUT : 'restart_diffusion_test_steady.i',
-             EXODIFF : ['steady_out.e']}
-    test2 = { INPUT : 'restart_diffusion_test_transient.i',
-            EXODIFF : ['out.e'],
-            PREREQ : 'test1' }
-
-except:
-    pass