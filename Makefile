idaholab/raven

Migrating applications to Modules!
r25428

Makefile
@@ -16,17 +16,14 @@ FRAMEWORK_DIR      ?= $(MOOSE_DIR)/framework
 CURR_DIR    := $(shell pwd)
 ROOT_DIR    := $(HERD_TRUNK_DIR)
 
-################################## ELK MODULES ################################
-ALL_ELK_MODULES := yes
-###############################################################################
-
 # framework
 include $(FRAMEWORK_DIR)/build.mk
 include $(FRAMEWORK_DIR)/moose.mk
 
-# modules
-ELK_DIR ?= $(HERD_TRUNK_DIR)/elk
-include $(ELK_DIR)/elk.mk
+################################## MODULES ####################################
+ALL_MODULES := yes
+include           $(MOOSE_DIR)/modules/modules.mk
+###############################################################################
 
 # dep apps
 APPLICATION_DIR    := $(HERD_TRUNK_DIR)/r7_moose

idaholab/falcon

Makefile
@@ -13,17 +13,15 @@ HERD_TRUNK_DIR     ?= $(shell dirname `pwd`)
 FRAMEWORK_DIR      ?= $(MOOSE_DIR)/framework
 ###############################################################################
 
-################################## ELK MODULES ################################
-ALL_ELK_MODULES := yes
-###############################################################################
-
 # framework
 include $(FRAMEWORK_DIR)/build.mk
 include $(FRAMEWORK_DIR)/moose.mk
 
-# TODO: Falcon STILL has several copies of Elk objects... This is sooooo wrong,
-# when will this ever get fixed?  You have list Falcon before Elk so that the 
-# proper includes will be found.  *sigh*
+################################## MODULES ####################################
+FLUID_MASS_ENERGY_BALANCE := yes
+CHEMICAL_REACTIONS        := yes
+include           $(MOOSE_DIR)/modules/modules.mk
+###############################################################################
 
 # dep apps
 APPLICATION_DIR    := $(HERD_TRUNK_DIR)/falcon
@@ -31,10 +29,6 @@ APPLICATION_NAME   := falcon
 BUILD_EXEC         := yes
 DEP_APPS           := $(shell $(FRAMEWORK_DIR)/scripts/find_dep_apps.py $(APPLICATION_NAME))
 include            $(FRAMEWORK_DIR)/app.mk
-
-# modules
-ELK_DIR ?= $(HERD_TRUNK_DIR)/elk
-include $(ELK_DIR)/elk.mk
 
 ###############################################################################
 # Additional special case targets should be added here