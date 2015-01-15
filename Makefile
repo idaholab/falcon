idaholab/raven

Revert "Makefile updates for all applications"
This reverts commit 6c0a07f68a69ea7e1c86c16c3615f1518c617886.

r24898

Makefile
@@ -13,7 +13,8 @@
 # Note: Make sure that there is no whitespace after the word 'yes' if enabling
 # an application
 ###############################################################################
-ROOT_DIR        ?= $(shell dirname `pwd`)
+CURR_DIR    ?= $(shell pwd)
+ROOT_DIR    ?= $(shell dirname `pwd`)
 
 ifeq ($(MOOSE_DEV),true)
 	MOOSE_DIR ?= $(ROOT_DIR)/devel/moose
@@ -21,24 +22,25 @@ else
 	MOOSE_DIR ?= $(ROOT_DIR)/moose
 endif
 
+ELK_DIR     ?= $(ROOT_DIR)/elk
+R7_DIR     ?= $(ROOT_DIR)/r7_moose
+RAVEN_DIR   ?= $(ROOT_DIR)/raven
+
+APPLICATION_NAME := RAVEN
+
+DEP_APPS    ?= $(shell $(MOOSE_DIR)/scripts/find_dep_apps.py $(APPLICATION_NAME))
+#ADDITIONAL_LIBS  += -lgsl
+
 ################################## ELK MODULES ################################
 ALL_ELK_MODULES := yes
 ###############################################################################
 
-# framework
+
 include $(MOOSE_DIR)/build.mk
+# deps
 include $(MOOSE_DIR)/moose.mk
-
-# modules
-ELK_DIR ?= $(ROOT_DIR)/elk
 include $(ELK_DIR)/elk.mk
-
-# dep apps
-APPLICATION_DIR    := $(ROOT_DIR)/r7_moose
-APPLICATION_NAME   := r7_moose
-DEP_APPS           := $(shell $(MOOSE_DIR)/scripts/find_dep_apps.py $(APPLICATION_NAME))
-include            $(MOOSE_DIR)/app.mk
-
+include $(R7_DIR)/r7.mk
 include $(RAVEN_DIR)/config.mk
 include $(RAVEN_DIR)/raven.mk
 include $(RAVEN_DIR)/raven_python_modules.mk

idaholab/falcon

Makefile
@@ -13,13 +13,20 @@
 # Note: Make sure that there is no whitespace after the word 'yes' if enabling
 # an application
 ###############################################################################
-ROOT_DIR        ?= $(shell dirname `pwd`)
+CURR_DIR   ?= $(shell pwd)
+ROOT_DIR   ?= $(shell dirname `pwd`)
 
 ifeq ($(MOOSE_DEV),true)
 	MOOSE_DIR ?= $(ROOT_DIR)/devel/moose
 else
 	MOOSE_DIR ?= $(ROOT_DIR)/moose
 endif
+ELK_DIR     ?= $(ROOT_DIR)/elk
+FALCON_DIR  ?= $(ROOT_DIR)/falcon
+
+APPLICATION_NAME := falcon
+
+DEP_APPS    ?= $(shell $(MOOSE_DIR)/scripts/find_dep_apps.py $(APPLICATION_NAME))
 
 ################################## ELK MODULES ################################
 FLUID_MASS_ENERGY_BALANCE := yes
@@ -27,25 +34,10 @@ SOLID_MECHANICS := no
 CHEMICAL_REACTIONS := yes
 ###############################################################################
 
-# framework
 include $(MOOSE_DIR)/build.mk
 include $(MOOSE_DIR)/moose.mk
-
-# TODO: Falcon STILL has several copies of Elk objects... This is sooooo wrong,
-# when will this ever get fixed?  You have list Falcon before Elk so that the 
-# proper includes will be found.  *sigh*
-
-# dep apps
-APPLICATION_DIR    := $(ROOT_DIR)/falcon
-APPLICATION_NAME   := falcon
-BUILD_EXEC         := yes
-DEP_APPS           := $(shell $(MOOSE_DIR)/scripts/find_dep_apps.py $(APPLICATION_NAME))
-include            $(MOOSE_DIR)/app.mk
-
-# modules
-ELK_DIR ?= $(ROOT_DIR)/elk
 include $(ELK_DIR)/elk.mk
-
+include $(FALCON_DIR)/falcon.mk
 
 ###############################################################################
 # Additional special case targets should be added here