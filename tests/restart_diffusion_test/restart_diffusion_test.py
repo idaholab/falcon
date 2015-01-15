idaholab/moose

Fixing PREREQ option in TestHarness - closes #1273
r12476

test/tests/restart_diffusion_test/restart_diffusion_test.py
@@ -5,7 +5,7 @@ test_1 = { INPUT : 'restart_diffusion_test_steady.i',
 
 test_2 = { INPUT : 'restart_diffusion_test_transient.i',
            EXODIFF : ['out.e'],
-           PREREQ : 'test_1' }
+           PREREQ : ['test_1'] }
 
 uniform_refine_test_1 = {
   INPUT : 'exodus_refined_restart_1_test.i',
@@ -15,12 +15,12 @@ uniform_refine_test_1 = {
 uniform_refine_test_2 = {
   INPUT : 'exodus_refined_restart_2_test.i',
   EXODIFF : ['exodus_refined_restart_2.e'],
-  PREREQ : 'uniform_refine_test_1'
+  PREREQ : ['uniform_refine_test_1']
 }
 
 uniform_refine_refine_test_2 = {
   INPUT : 'exodus_refined_refined_restart_2_test.i',
   EXODIFF : ['exodus_refined_refined_restart_2.e'],
-  PREREQ : 'uniform_refine_test_1',
+  PREREQ : ['uniform_refine_test_1'],
 }
 

idaholab/falcon

tests/restart_diffusion_test/restart_diffusion_test.py
@@ -4,5 +4,5 @@ test1 = { INPUT : 'restart_diffusion_test_steady.i',
          EXODIFF : ['steady_out.e']}
 test2 = { INPUT : 'restart_diffusion_test_transient.i',
         EXODIFF : ['out.e'],
-        PREREQ : 'test1' }
+        PREREQ : ['test1'] }
 
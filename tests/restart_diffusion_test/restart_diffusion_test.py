7ffd99edfa0970ae5be8624cf2e10dc36043c431
Cody Permann <cody.permann@inl.gov>
2012-08-02 15:41:37 +0000

    Fixing PREREQ option in TestHarness - closes #1273
    r12476

--- tests/restart_diffusion_test/restart_diffusion_test.py
+++ tests/restart_diffusion_test/restart_diffusion_test.py
@@ -4,5 +4,5 @@ test1 = { INPUT : 'restart_diffusion_test_steady.i',
          EXODIFF : ['steady_out.e']}
 test2 = { INPUT : 'restart_diffusion_test_transient.i',
         EXODIFF : ['out.e'],
-        PREREQ : 'test1' }
+        PREREQ : ['test1'] }
 
