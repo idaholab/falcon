idaholab/moose

Enabling tests to run threaded
r4053

test/tests/2d_diffusion_test/2d_diffusion_test.py
@@ -1,7 +1,7 @@
 import tools
 
-def testdirichlet(dofs=0, np=0):
-  tools.executeAppAndDiff(__file__,'2d_diffusion_test.i',['out.e'], dofs, np)
+def testdirichlet(dofs=0, np=0, n_threads=0):
+  tools.executeAppAndDiff(__file__,'2d_diffusion_test.i',['out.e'], dofs, np, n_threads)
 
-def testneumann(dofs=0, np=0):
-  tools.executeAppAndDiff(__file__,'2d_diffusion_neumannbc_test.i',['neumannbc_out.e'], dofs, np)
+def testneumann(dofs=0, np=0, n_threads=0):
+  tools.executeAppAndDiff(__file__,'2d_diffusion_neumannbc_test.i',['neumannbc_out.e'], dofs, np, n_threads)

idaholab/falcon

tests/2d_diffusion_test/2d_diffusion_test.py
@@ -1,4 +1,4 @@
 import tools
 
-def test():
-  tools.executeAppAndDiff(__file__,'2d_diffusion_test.i',['out.e'])
+def test(dofs=0, np=0, n_threads=0):
+  tools.executeAppAndDiff(__file__,'2d_diffusion_test.i',['out.e'], dofs, np, n_threads)
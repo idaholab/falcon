c802cbe7e49814d68e4a72929a99ca4a80c4bd2a
David Andrs <david.andrs@inl.gov>
2011-03-28 14:16:33 +0000

    Enabling tests to run threaded
    r4053

--- tests/CDE_test/CDE_test.py
+++ tests/CDE_test/CDE_test.py
@@ -1,5 +1,5 @@
 import tools
 
-def test():
+def test(dofs=0, np=0, n_threads=0):
  # pass
-  tools.executeAppAndDiff(__file__,'pressure_temp_1d_x_test.i',['out.e'])
+  tools.executeAppAndDiff(__file__,'pressure_temp_1d_x_test.i',['out.e'], dofs, np, n_threads)
