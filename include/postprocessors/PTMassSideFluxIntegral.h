3cc3e95b7093c0a6a902f3a83ca652c6ffd20115
Yidong Xia <yidong.xia@inl.gov>
2015-12-07 17:09:39 -0700

    Added postprocessors PT_H_SideFluxIntegral and test case

---
+++ include/postprocessors/PTMassSideFluxIntegral.h
@@ -0,0 +1,41 @@
+/****************************************************************/
+/*             DO NOT MODIFY OR REMOVE THIS HEADER              */
+/*          FALCON - Fracturing And Liquid CONvection           */
+/*                                                              */
+/*       (c)     2012 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#ifndef PTMASSSIDEFLUXINTEGRAL_H
+#define PTMASSSIDEFLUXINTEGRAL_H
+
+#include "SideIntegralVariablePostprocessor.h"
+#include "MaterialPropertyInterface.h"
+#include "Material.h"
+
+//Forward Declarations
+class PTMassSideFluxIntegral;
+
+template<>
+InputParameters validParams<PTMassSideFluxIntegral>();
+
+class PTMassSideFluxIntegral : public SideIntegralVariablePostprocessor
+{
+  public:
+
+    PTMassSideFluxIntegral(const InputParameters & parameters);
+
+  protected:
+
+    virtual Real computeQpIntegral();
+
+    const MaterialProperty<RealGradient> & _wdmfx;
+};
+
+#endif // PTMASSSIDEFLUXINTEGRAL_H
---
+++ src/postprocessors/PTMassSideFluxIntegral.C
@@ -0,0 +1,52 @@
+/****************************************************************/
+/*             DO NOT MODIFY OR REMOVE THIS HEADER              */
+/*          FALCON - Fracturing And Liquid CONvection           */
+/*                                                              */
+/*       (c)     2012 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+/*----------------------------------------------------------------
+
+Contributors: Yidong Xia (INL)
+
+Descriptions: calculate P-T based total mass flux on a side
+
+----------------------------------------------------------------*/
+
+#include "PTMassSideFluxIntegral.h"
+
+template<>
+InputParameters validParams<PTMassSideFluxIntegral>()
+{
+  InputParameters params = validParams<SideIntegralVariablePostprocessor>();
+  return params;
+}
+
+
+/*******************************************************************************
+Routine: PTMassSideFluxIntegral --- constructor
+Authors: Yidong Xia
+*******************************************************************************/
+PTMassSideFluxIntegral::PTMassSideFluxIntegral(const InputParameters & parameters):
+  SideIntegralVariablePostprocessor(parameters),
+  _wdmfx(getMaterialProperty<RealGradient>("darcy_mass_flux_water"))
+{}
+
+
+/*******************************************************************************
+Routine: computeQpResidual
+Authors: Yidong Xia
+*******************************************************************************/
+Real
+PTMassSideFluxIntegral::computeQpIntegral()
+{
+  return _wdmfx[_qp]*_normals[_qp];
+}
+
