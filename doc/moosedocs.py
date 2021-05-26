7689ef586f5b247680f4762256754557a210ff7a
crswong888 <crswong888@gmail.com>
2021-05-26 14:42:02 -0600

    Fixed incorrect path to MOOSE submodule
    (refs #43 idaholab/moose#17951)

--- doc/moosedocs.py
+++ doc/moosedocs.py
@@ -2,24 +2,29 @@
 import sys
 import os
 
-# Locate MOOSE directory
-MOOSE_DIR = os.getenv('MOOSE_DIR', os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..')))
-if not os.path.exists(MOOSE_DIR):
-    MOOSE_DIR = os.path.abspath(os.path.join(os.path.dirname(__name__), '..', '..', 'moose'))
-if not os.path.exists(MOOSE_DIR):
+# Locate MOOSE directory - first check if MOOSE_DIR env. is already set, if not, check for submodule
+MOOSE_SUBMODULE_DIR = os.path.abspath(os.path.join(os.path.dirname(__name__), '..', 'moose'))
+MOOSE_DIR = os.getenv('MOOSE_DIR', MOOSE_SUBMODULE_DIR)
+
+# If MOOSE_DIR env. is not set and there's no submodule, check the parent directory of FALCON
+if not os.path.isdir(MOOSE_DIR):
+    MOOSE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'moose'))
+if not os.path.isdir(MOOSE_DIR):
     print('Failed to locate MOOSE, specify the MOOSE_DIR environment variable.')
     sys.exit(1)
 os.environ['MOOSE_DIR'] = MOOSE_DIR
 
-# Locate FALCON root in environment variables, otherwise, set path relative to 'falcon/doc'
-# if 'FALCON_DIR' not in os.environ:
-#     os.environ['FALCON_DIR'] = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-
-# Append MOOSE python directory
+# Append the MOOSE python directory to path so that modules can be imported
 MOOSE_PYTHON_DIR = os.path.join(MOOSE_DIR, 'python')
 if MOOSE_PYTHON_DIR not in sys.path:
     sys.path.append(MOOSE_PYTHON_DIR)
 
+# If using the MOOSE submodule, it needs to be initialized
+if MOOSE_DIR == MOOSE_SUBMODULE_DIR:
+    import mooseutils
+    mooseutils.git_init_submodule(os.path.relpath(MOOSE_DIR))
+
+# Import and run MooseDocs
 from MooseDocs import main
 if __name__ == '__main__':
     sys.exit(main.run())
