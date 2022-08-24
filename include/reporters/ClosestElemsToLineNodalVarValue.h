d44814d0937b9cf9581e346b4218f0daa4acf972
Lynn Munday <lynn.munday@inl.gov>
2022-08-23 21:22:45 -0600

    reporter that reports the nodal variable values at the elements located by the nearestElemsToLine.  Also renamed the variable used to for filtering elements and updated tests. closes #108

---
+++ include/reporters/ClosestElemsToLineNodalVarValue.h
@@ -0,0 +1,33 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "ClosestElemsToLine.h"
+#include "MooseVariableInterface.h"
+
+class ClosestElemsToLineNodalVarValue : public ClosestElemsToLine,
+                                        public MooseVariableInterface<Real>
+{
+public:
+  static InputParameters validParams();
+
+  ClosestElemsToLineNodalVarValue(const InputParameters & parameters);
+
+  virtual void initialSetup() override;
+
+  virtual void initialize() override{};
+  virtual void execute() override;
+  virtual void finalize() override;
+
+private:
+  std::string _var_name;
+  MooseVariable & _var;
+  std::vector<Real> & _value;
+};
