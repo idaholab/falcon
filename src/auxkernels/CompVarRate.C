6f7cd17db02cf91f0e66a53baf035bbbc5a1d64c
chunhuizhao478 <chunhui3@illinois.edu>
2023-04-04 21:49:45 -0500

    initial-commit

---
+++ src/auxkernels/CompVarRate.C
@@ -0,0 +1,32 @@
+/*
+AuxKernel of Passing Variable Time Derivative 
+*/
+
+#include "CompVarRate.h"
+
+registerMooseObject("farmsApp", CompVarRate);
+
+InputParameters
+CompVarRate::validParams()
+{
+  InputParameters params = AuxKernel::validParams();
+
+  params.addRequiredCoupledVar("coupled","Nonlinear Variable that needed to be taken time derivative of");
+
+  return params;
+}
+
+CompVarRate::CompVarRate(const InputParameters & parameters)
+  : AuxKernel(parameters),
+  
+  //Compute the time derivative of the given variable using "coupledDot"
+  _coupled_val(coupledDot("coupled"))
+
+{
+}
+
+Real
+CompVarRate::computeValue()
+{
+  return _coupled_val[_qp];
+}
