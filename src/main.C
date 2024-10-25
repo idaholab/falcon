533f8c30399c77454939b40fe1095ca3b9f631aa
Logan Harbour <loganhharbour@gmail.com>
2024-10-25 08:03:39 -0600

    Use new moose main
    refs idahlab/moose#28639

--- src/main.C
+++ src/main.C
@@ -12,28 +12,11 @@
 /*            See COPYRIGHT for full restrictions               */
 /****************************************************************/
 
-/****************************************************************/
-/*             DO NOT MODIFY OR REMOVE THIS HEADER              */
-/*          FALCON - Fracturing And Liquid CONvection           */
-/*                                                              */
-/*       (c) pending 2012 Battelle Energy Alliance, LLC         */
-/*                   ALL RIGHTS RESERVED                        */
-/*                                                              */
-/*          Prepared by Battelle Energy Alliance, LLC           */
-/*            Under Contract No. DE-AC07-05ID14517              */
-/*            With the U. S. Department of Energy               */
-/*                                                              */
-/*            See COPYRIGHT for full restrictions               */
-/****************************************************************/
-
 #include "FalconTestApp.h"
 #include "MooseMain.h"
 
-// Begin the main program.
 int
 main(int argc, char * argv[])
 {
-  Moose::main<FalconTestApp>(argc, argv);
-
-  return 0;
+  return Moose::main<FalconTestApp>(argc, argv);
 }
